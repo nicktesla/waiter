a1405850779c0beb60b0e68b343f4e02425f1b9c
nicktesla <nickteslacodes@gmail.com>
2014-07-27 22:19:29 -0700

    multi server queue...quick and dirty

--- demo.js
+++ demo.js
@@ -1,13 +1,6 @@
 waiter = require('./lib/waiter')
 
 
-/**
-for(var i=1; i < 10; i++) {
-  var sum = 0;
-  for(j=0; j <100000; j++) {
-    var wt = waiter.waitTime(100, 100, 1000*i, 1, 2); //meanTime, hourlyDemand, speed, min, max
-    sum += wt;
-  }
-  console.log("the wait time for speed: ", 1000*i, " is: ", wt/100000);
-}
-**/
+//waitTime = function(meanArrTime, hourlyDemand, hourlySupply, speed, min, max, runCount) {
+
+var wt = waiter.waitTime(1, 100, 80, 1, 1, 60, 100000);
