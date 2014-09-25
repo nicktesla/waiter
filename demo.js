afbc97ab846a93a70c436900b9e41b70b5b4b876
nicktesla <nickteslacodes@gmail.com>
2014-09-25 11:01:46 -0700

    push latest

--- demo.js
+++ demo.js
@@ -2,5 +2,8 @@ waiter = require('./lib/waiter');
 
 
 //waitTime = function(meanArrTime, hourlyDemand, hourlySupply, speed, min, max, runCount) {
+//supply = function (meanArrTime, hourlyDemand, maxWaitTime, speed, min, max, runCount) {
 
 var wt = waiter.waitTime(1, 100, 80, 1, 1, 60, 100000);
+
+var s = waiter.supply(1, 100, 5, 1, 1, 60, 100000);
